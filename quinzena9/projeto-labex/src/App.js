ebb74a8e37cfa4dd585dbdceee5af8d466ac89ce
Jonathan-Andrade <sjonatask@gmail.com>
2022-01-18 22:13:50 -0300

    Finalizado HomePage e renderização das viagens

---
+++ quinzena9/projeto-labex/src/App.js
@@ -0,0 +1,13 @@
+// import axios from "axios";
+import React, { useState, useEffect } from "react"
+import HomePage from "./Pages/HomePage/HomePage.js"
+import Lista from "./Pages/ListTripPage/ListTripsPage"
+
+function App() {
+  return (
+    // <HomePage/>
+    <Lista/>
+  );
+}
+
+export default App;
