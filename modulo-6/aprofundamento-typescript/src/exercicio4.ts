4a81470ea94750759bcca6b3eeb2fde8055c3d01
Jonathan-Andrade <sjonatask@gmail.com>
2022-03-24 23:21:18 -0300

    finalização

---
+++ modulo-6/aprofundamento-typescript/src/exercicio4.ts
@@ -0,0 +1,28 @@
+type pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
+
+// B) tsc exercicio4.ts
+// C) tsc src/exercicio4.ts
+// D) no caso poderiamos colocar o nome dos arquivos separados por exemplo tsc arquivo1 arquivo2 e assim por andiante
+
