4a1c2c9aae7e11dd2e853330bcb43a8d9e6bfa25
Jonathan-Andrade <sjonatask@gmail.com>
2022-04-05 23:26:59 -0300

    atividade ate a 6

---
+++ modulo-6/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,23 @@
+type Products = {
+    id: string,
+    name: string,
+    price: number
+}[]
+
+export const products: Products = [
+    {
+        id: "dadaedasfwe-sdawdsge",
+        name: "banana",
+        price: 2
+    },
+    {
+        id: "h21u3h21g-2odko21dm",
+        name: "pijama",
+        price: 20
+    },
+    {
+        id: "sjeisajd-adiwjda2",
+        name: "escada",
+        price: 120
+    }
+]
