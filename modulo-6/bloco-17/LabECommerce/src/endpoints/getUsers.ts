3d17041f1a2dcc1f5005e6b0dc66d1821b22e81a
Jonathan-Andrade <sjonatask@gmail.com>
2022-05-05 18:59:35 -0300

    finalizando desafio de imrpimir compras dos usuarios no endpoint que chama todos os usuarios

--- modulo-6/bloco-17/LabECommerce/src/endpoints/getUsers.ts
+++ modulo-6/bloco-17/LabECommerce/src/endpoints/getUsers.ts
@@ -1,11 +1,12 @@
 import { Express, Request, Response } from "express"
+import addUserPurchase from "./querys/addUserPurchase"
 import { findUser } from "./querys/findUser"
 
 export const getUsers = async(req: Request,res: Response): Promise<void> =>{
     let errorCode = 400
     try {
-    
-        let result = await findUser()
+        
+        let result = await findUser().then(addUserPurchase)
 
         res.status(200).send(result)
     } catch (error:any) {
--- modulo-6/bloco-17/LabECommerce/src/type.ts
+++ modulo-6/bloco-17/LabECommerce/src/type.ts
@@ -3,6 +3,7 @@ export type User = {
     name: string,
     email: string,
     password: string
+    purchase?: [{}]
 }
 
 export type Product ={
